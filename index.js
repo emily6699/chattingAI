edd87fccf6c690ebf2a18ab9fa615040338806ae
emily <emily@emilys-MacBook-Pro.local>
2019-04-25 18:04:29 -0400

    issues with applying css

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+'use strict';
+
+require('dotenv').config()
+
+
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/views')); //html
 app.use(express.static(__dirname + '/publice')); //js, css, images
 
-const server = app.listen(5000);
+const server = app.listen(process.env.PORT || 5000, () => {
+    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
 
+const io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    console.log('a user connected')
+})
 
-const apiai = require('apiai')(bbf75702f8f143a69c5a6bada55eb814)
+const apiai = require('apiai')('bbf75702f8f143a69c5a6bada55eb814')
 
+//Web UI
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
 
-import.on('connection', function (socket) {
+io.on('connection', function (socket) {
     socket.on('chat message', (test) => {
+        console.log('Message: ' + text)
         // get a reply from API.AI
 
         let apiaiReq = apiai.textRequest(test, {
--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,59 @@
+'use strict';
+
 const socket = io();
+
+const outputYou = document.querySelector('.output-you');
+const outputBot = document.querySelector('.output-bot');
+
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
 recognition.lang = 'en-US';
 recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
-document.querySelector('button').addEventListener('click', ()=>{
+document.querySelector('button').addEventListener('click', () => {
     recognition.start();
 });
 
+recognition.addEventListener('speechstart', () => {
+    console.log('Speech has been detected.');
+});
 
-recognition.addEventListener('result', (e)=>{
-    let last = e.results.length-1;
+
+recognition.addEventListener('result', (e) => {
+    console.log('Result has been detected.')
+
+    let last = e.results.length - 1;
     let text = e.results[last][0].transcript;
 
+    outputYou.textContent = text;
     console.log('Confidence: ' + e.resultes[0][0].confidence);
 
     // we will use Socket.IO here later...
     socket.emit('chat message', text);
-})
+});
+
+recognition.addEventListener('speechend', () => {
+    recognition.stop();
+});
+
+recognition.addEventListener('error', (e) => {
+    outputBot.textContent = 'Error: ' + e.error;
+
+});
+
+function synthVoice(text) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    synth.speack(utterance);
+}
+
+socket.on('Robot2019 said', function (replyText) {
+    synthVoice(replyText);
+
+    if (replyText == '') replyText = '(No answer...)';
+    outputBot.textContent = replyText;
+});
